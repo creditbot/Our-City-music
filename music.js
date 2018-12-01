ed5ac0206db2693d2e8518f4a41bbe6a225c4ff7
creditbot <44919914+creditbot@users.noreply.github.com>
2018-12-01 17:37:25 +0300

    Add files via upload

--- music.js
+++ music.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+﻿const Discord = require('discord.js');
 const client = new Discord.Client();
 const convert = require("hh-mm-ss")
 const dateFormat = require('dateformat');
